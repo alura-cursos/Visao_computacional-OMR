a45016a216a1dc3e26bd82a8e250833058323edd
Suayder <suayder@uft.edu.br>
2020-02-06 00:22:50 -0300

    mophological operations

---
+++ Makefile
@@ -0,0 +1,2 @@
+run:
+	python correcao_provas.py images-test/teste0.png
--- correcao_provas.py
+++ correcao_provas.py
@@ -9,18 +9,24 @@ def main():
 
     # tratamento de erros que podem ocorrer devido ao caminho da imagem passado
 
-    if(len(sys.argv)<2):
+    if(len(sys.argv)!=2):
         print('\n\ncompile da seguinte forma: \'python nome_do_seu_programa.py caminho_e_nome_da_imagem\'\n\n')
         return 0
 
-    img_original = cv2.imread(sys.argv[1])
+    img_original = cv2.imread(sys.argv[1],0)
 
-    if(img_original.any()==None):
+    if(img_original is None):
         print('\n\nO OpenCV não conseguiu abrir a imagem. Verifique o caminho que você passou ou se a imagem existe.\n\n')
         return 0
 
     mostrar(img_original)
-    
+
+    img_suavizada = cv2.GaussianBlur(img_original, (5,5), 0)
+    img_binarizada, thres = cv2.threshold(img_suavizada, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+    operacao_closing = cv2.morphologyEx(img_binarizada, cv2.MORPH_CLOSE, (5,5))
+    print(operacao_closing.shape)
+    mostrar(operacao_closing)
 
 if __name__=='__main__':
     main()
